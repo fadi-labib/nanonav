981e747fbdc1acabed0dfb800dbc7980b0fc70b3
Fadi Labib <github@fadilabib.com>
2025-11-24 09:15:00 +0100

    Add project setup: gitignore, requirements, package init

---
+++ trm_nav/__init__.py
@@ -0,0 +1,12 @@
+"""
+TRM-Nav: Tiny Recursive Model for Local Robot Navigation
+
+A proof-of-concept implementation comparing TRM to A* for grid navigation.
+"""
+
+__version__ = "0.1.0"
+
+from .a_star import astar_path
+from .map_generator import generate_random_map, generate_solvable_map
+from .dataset import NavigationDataset, build_dataset
+from .model import TRMNavigator
